464a7f085501f40b3229124b348ed2b98d437eca
LufatZ <86516373+LufatZ@users.noreply.github.com>
2022-04-02 13:38:55 +0200

    Farmland working in the most cases now

--- src/main/java/de/dayofmind/additions/Additions.java
+++ src/main/java/de/dayofmind/additions/Additions.java
@@ -3,11 +3,9 @@ package de.dayofmind.additions;
 import de.dayofmind.additions.block.DOMBlocks;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.block.*;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 /*
 TODO:
