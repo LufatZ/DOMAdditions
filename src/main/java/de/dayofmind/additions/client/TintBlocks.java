465ab41f9c66c4732602fefec394cf44d1c91290
LufatZ <86516373+LufatZ@users.noreply.github.com>
2022-05-04 19:09:08 +0200

    rewrite TintBlocks and changed particle of grass variants to top texture... this isn´t really a fix for (unwanted) tinted Particle, but it looks better

--- src/main/java/de/dayofmind/additions/client/TintBlocks.java
+++ src/main/java/de/dayofmind/additions/client/TintBlocks.java
@@ -1,19 +1,21 @@
 package de.dayofmind.additions.client;
 
-import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.color.world.BiomeColors;
 import net.minecraft.client.color.world.GrassColors;
 
 import static de.dayofmind.additions.block.DOMBlocks.GRASS_SLAB;
 import static de.dayofmind.additions.block.DOMBlocks.GRASS_STAIR;
+import static net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry.ITEM;
+import static net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry.BLOCK;
 
 public class TintBlocks {
+
+
     public static void TintGrassBlocks() {
-        //slab
-        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getGrassColor(world, pos) : GrassColors.getColor(0.5D, 1.0D), GRASS_SLAB);
-        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> GrassColors.getColor(0.5D, 1.0D), GRASS_SLAB);
-        //stair
-        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getGrassColor(world, pos) : GrassColors.getColor(0.5D, 1.0D), GRASS_STAIR);
-        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> GrassColors.getColor(0.5D, 1.0D), GRASS_STAIR);
+
+        //grass block variants
+        BLOCK.register((state, world, pos, tintIndex) -> world != null && pos != null ? BiomeColors.getGrassColor(world, pos) : GrassColors.getColor(0.5, 1.0), GRASS_SLAB, GRASS_STAIR);
+        ITEM.register((stack, tintIndex) -> GrassColors.getColor(0.5D, 1.0D), GRASS_SLAB, GRASS_STAIR);
     }
+
 }
