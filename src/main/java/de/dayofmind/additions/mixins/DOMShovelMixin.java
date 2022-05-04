8c3cf60192c824fbb98475f650b69b95c50dcd8c
LufatZ <86516373+LufatZ@users.noreply.github.com>
2022-05-04 13:54:34 +0200

    rewrite of DOMShovelMixin

--- src/main/java/de/dayofmind/additions/mixins/DOMShovelMixin.java
+++ src/main/java/de/dayofmind/additions/mixins/DOMShovelMixin.java
@@ -14,7 +14,6 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,76 +21,54 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 
-import static net.minecraft.block.StairsBlock.*;
+import static net.minecraft.block.StairsBlock.SHAPE;
+import static net.minecraft.block.StairsBlock.WATERLOGGED;
 
 @Mixin(ShovelItem.class)
 public class DOMShovelMixin {
-    private static final Map<Block, BlockState> PATH_STATES = Maps.newHashMap(new ImmutableMap.Builder<Block, BlockState>().put(Blocks.GRASS_BLOCK, Blocks.DIRT_PATH.getDefaultState()).put(Blocks.DIRT, Blocks.DIRT_PATH.getDefaultState()).put(Blocks.PODZOL, Blocks.DIRT_PATH.getDefaultState()).put(Blocks.COARSE_DIRT, Blocks.DIRT_PATH.getDefaultState()).put(Blocks.MYCELIUM, Blocks.DIRT_PATH.getDefaultState()).put(Blocks.ROOTED_DIRT, Blocks.DIRT_PATH.getDefaultState()).build());
-    private static final Map<Block, BlockState>  PATH_STATES2 = Maps.newHashMap(new ImmutableMap.Builder<Block, BlockState>().put(DOMBlocks.DIRT_SLAB, DOMBlocks.DIRT_PATH_SLAB.getDefaultState()).put(DOMBlocks.GRASS_SLAB, DOMBlocks.DIRT_PATH_SLAB.getDefaultState()).build());
-    private static final Map<Block, BlockState>  PATH_STATES3 = Maps.newHashMap(new ImmutableMap.Builder<Block, BlockState>().put(DOMBlocks.DIRT_STAIR, DOMBlocks.DIRT_PATH_STAIR.getDefaultState()).put(DOMBlocks.GRASS_STAIR, DOMBlocks.DIRT_PATH_STAIR.getDefaultState()).build());
+    private static final Map<Block, BlockState> PATH_STATES2 = Maps.newHashMap(new ImmutableMap.Builder<Block, BlockState>().put(DOMBlocks.DIRT_SLAB, DOMBlocks.DIRT_PATH_SLAB.getDefaultState()).put(DOMBlocks.GRASS_SLAB, DOMBlocks.DIRT_PATH_SLAB.getDefaultState()).put(DOMBlocks.DIRT_STAIR, DOMBlocks.DIRT_PATH_STAIR.getDefaultState()).put(DOMBlocks.GRASS_STAIR, DOMBlocks.DIRT_PATH_STAIR.getDefaultState()).build());
 
-    //TODO scramble and optimize (possibly register or change new tags)
-
-    @Inject(at = @At("HEAD"), method = "useOnBlock", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "useOnBlock")
     public void useOnBlock(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
         World world = context.getWorld();
         BlockPos blockPos = context.getBlockPos();
         BlockState blockState = world.getBlockState(blockPos);
-        if (context.getSide() != Direction.DOWN) {
+        if (context.getSide() != Direction.DOWN && world.getBlockState(blockPos).isIn(BlockTags.DIRT)&& (blockState.getBlock() instanceof StairsBlock || blockState.getBlock() instanceof SlabBlock)) {
             PlayerEntity playerEntity = context.getPlayer();
-            BlockState blockState2 = PATH_STATES.get(blockState.getBlock());
-            BlockState blockState3 = null;
-            if (blockState2 != null && world.getBlockState(blockPos.up()).isAir()) {
-                world.playSound(playerEntity, blockPos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0f, 1.0f);
-                blockState3 = blockState2;
-            } else if (blockState.getBlock() instanceof CampfireBlock && blockState.get(CampfireBlock.LIT).booleanValue()) {
+            BlockState blockState4 = null;
+            if(blockState.getBlock() instanceof StairsBlock){
+                blockState4 = PATH_STATES2.get(blockState.getBlock())
+                        .with(StairsBlock.FACING, blockState.get(StairsBlock.FACING))
+                        .with(StairsBlock.HALF, blockState.get(StairsBlock.HALF))
+                        .with(StairsBlock.SHAPE, blockState.get(SHAPE))
+                        .with(WATERLOGGED, blockState.get(WATERLOGGED));
+            }
+            if(blockState.getBlock() instanceof SlabBlock){
+                blockState4 = PATH_STATES2.get(blockState.getBlock())
+                        .with(SlabBlock.TYPE, blockState.get(SlabBlock.TYPE))
+                        .with(WATERLOGGED, blockState.get(WATERLOGGED));
+            }
+            BlockState blockState5 = null;
+
+            if (blockState4 != null && world.getBlockState(blockPos.up()).isAir()) {
+                world.playSound(playerEntity, blockPos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                blockState5 = blockState4;
+            } else if (blockState.getBlock() instanceof CampfireBlock && (Boolean)blockState.get(CampfireBlock.LIT)) {
                 if (!world.isClient()) {
-                    world.syncWorldEvent(null, WorldEvents.FIRE_EXTINGUISHED, blockPos, 0);
+                    world.syncWorldEvent((PlayerEntity)null, 1009, blockPos, 0);
                 }
+
                 CampfireBlock.extinguish(context.getPlayer(), world, blockPos, blockState);
-                blockState3 = blockState.with(CampfireBlock.LIT, false);
+                blockState5 = (BlockState)blockState.with(CampfireBlock.LIT, false);
             }
-            if (blockState3 != null) {
-                if (!world.isClient) {
-                    world.setBlockState(blockPos, blockState3, Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD);
-                    if (playerEntity != null) {
-                        context.getStack().damage(1, playerEntity, p -> p.sendToolBreakStatus(context.getHand()));
-                    }
-                }
-            }
-        }
-        if (context.getSide() != Direction.DOWN && blockState.getBlock() instanceof StairsBlock && world.getBlockState(blockPos).isIn(BlockTags.DIRT)) {
-            BlockState blockState4 = PATH_STATES3.get(blockState.getBlock()).with(FACING, blockState.get(FACING)).with(HALF, blockState.get(StairsBlock.HALF)).with(StairsBlock.WATERLOGGED, blockState.get(StairsBlock.WATERLOGGED)).with(SHAPE, blockState.get(SHAPE));
-            BlockState blockState5 = null;
-            PlayerEntity playerEntity = context.getPlayer();
 
-            if (blockState4 != null && world.getBlockState(blockPos.up()).isAir()) {
-                world.playSound(playerEntity, blockPos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0f, 1.0f);
-                blockState5 = blockState4;
-            }
             if (blockState5 != null) {
                 if (!world.isClient) {
-                    world.setBlockState(blockPos, blockState5, Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD);
+                    world.setBlockState(blockPos, blockState5, 11);
                     if (playerEntity != null) {
-                        context.getStack().damage(1, playerEntity, p -> p.sendToolBreakStatus(context.getHand()));
-                    }
-                }
-            }
-        }
-        if (context.getSide() != Direction.DOWN && blockState.getBlock() instanceof SlabBlock && world.getBlockState(blockPos).isIn(BlockTags.DIRT)) {
-            BlockState blockState4 = PATH_STATES2.get(blockState.getBlock()).with(SlabBlock.TYPE, blockState.get(SlabBlock.TYPE)).with(SlabBlock.WATERLOGGED, blockState.get(SlabBlock.WATERLOGGED));
-            BlockState blockState5 = null;
-            PlayerEntity playerEntity = context.getPlayer();
-
-            if (blockState4 != null && world.getBlockState(blockPos.up()).isAir()) {
-                world.playSound(playerEntity, blockPos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0f, 1.0f);
-                blockState5 = blockState4;
-            }
-            if (blockState5 != null) {
-                if (!world.isClient) {
-                    world.setBlockState(blockPos, blockState5, Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD);
-                    if (playerEntity != null) {
-                        context.getStack().damage(1, playerEntity, p -> p.sendToolBreakStatus(context.getHand()));
+                        context.getStack().damage(1, playerEntity, (p) -> {
+                            p.sendToolBreakStatus(context.getHand());
+                        });
                     }
                 }
             }
