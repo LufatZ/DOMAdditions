4f49e11b210bb6bb5ef701d34213fd463acca88f
LufatZ <86516373+LufatZ@users.noreply.github.com>
2023-02-18 13:06:36 +0100

    makes no sence

--- src/main/java/de/dayofmind/additions/block/stairs/DOMStairs.java
+++ src/main/java/de/dayofmind/additions/block/stairs/DOMStairs.java
@@ -3,11 +3,10 @@ package de.dayofmind.additions.block.stairs;
 import de.dayofmind.additions.block.DOMBlocksRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.StairsBlock;
-import net.minecraft.block.Waterloggable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class DOMStairs extends StairsBlock implements Waterloggable {
+public class DOMStairs extends StairsBlock {
     public DOMStairs(BlockState baseBlockState, Settings settings) {
         super(baseBlockState, settings);
     }
