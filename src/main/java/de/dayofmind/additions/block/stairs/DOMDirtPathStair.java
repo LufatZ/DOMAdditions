59085a254266c0a09693296ed47396ee10b2e64a
LufatZ <86516373+LufatZ@users.noreply.github.com>
2023-02-18 12:36:27 +0100

    Dirt path stair placement fixes
    same behavior like dirt path block

--- src/main/java/de/dayofmind/additions/block/stairs/DOMDirtPathStair.java
+++ src/main/java/de/dayofmind/additions/block/stairs/DOMDirtPathStair.java
@@ -1,25 +1,42 @@
 package de.dayofmind.additions.block.stairs;
 
 import de.dayofmind.additions.block.DOMBlocksRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.FenceGateBlock;
+import net.minecraft.block.enums.BlockHalf;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
 
 public class DOMDirtPathStair extends DOMShortStairs{
     public DOMDirtPathStair(BlockState baseBlockState, Settings settings) {
         super(baseBlockState, settings);
     }
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        Direction direction = ctx.getSide();
+        BlockPos blockPos = ctx.getBlockPos();
+        FluidState fluidState = ctx.getWorld().getFluidState(blockPos);
+        return !this.getDefaultState().canPlaceAt(ctx.getWorld(), ctx.getBlockPos()) ? Block.pushEntitiesUpBeforeBlockChange(this.getDefaultState(), DOMBlocksRegistry.DIRT_STAIR.getDefaultState().with(FACING, ctx.getPlayerFacing()).with(HALF, direction != Direction.DOWN && (direction == Direction.UP || !(ctx.getHitPos().y - (double)blockPos.getY() > 0.5)) ? BlockHalf.BOTTOM : BlockHalf.TOP).with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER), ctx.getWorld(), ctx.getBlockPos()) : super.getPlacementState(ctx);
+    }
     public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-        if (direction == Direction.UP) {
+        if (direction == Direction.UP && !state.canPlaceAt(world, pos)) {
             world.scheduleBlockTick(pos, this, 1);
         }
 
         return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
     public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-        world.setBlockState(pos, DOMBlocksRegistry.DIRT_STAIR.getDefaultState().with(FACING, state.get(FACING)).with(HALF, state.get(HALF)).with(SHAPE, state.get(SHAPE)).with(WATERLOGGED, state.get(WATERLOGGED)));
+       DOMStairs.setToDirtStair(state, world, pos);
+    }
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        BlockState blockState = world.getBlockState(pos.up());
+        return !blockState.getMaterial().isSolid() || blockState.getBlock() instanceof FenceGateBlock;
     }
 }
--- src/main/java/de/dayofmind/additions/block/stairs/DOMStairs.java
+++ src/main/java/de/dayofmind/additions/block/stairs/DOMStairs.java
@@ -1,13 +1,18 @@
 package de.dayofmind.additions.block.stairs;
 
+import de.dayofmind.additions.block.DOMBlocksRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.StairsBlock;
 import net.minecraft.block.Waterloggable;
-import net.minecraft.state.property.Properties;
-import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class DOMStairs extends StairsBlock implements Waterloggable {
     public DOMStairs(BlockState baseBlockState, Settings settings) {
         super(baseBlockState, settings);
+    }
+
+    public static void setToDirtStair(BlockState state, World world, BlockPos pos) {
+        world.setBlockState(pos, DOMBlocksRegistry.DIRT_STAIR.getDefaultState().with(FACING, state.get(FACING)).with(HALF, state.get(HALF)).with(SHAPE, state.get(SHAPE)).with(WATERLOGGED, state.get(WATERLOGGED)));
     }
 }
