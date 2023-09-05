05df151d7c5c03abaa18d7e0cec9b9af90da434e
lufatz <lucasdamme65@gmail.com>
2023-09-05 18:00:39 +0200

    simplify multiblocktool

--- src/main/java/de/dayofmind/additions/item/tools/DOMMultiBlockTool.java
+++ src/main/java/de/dayofmind/additions/item/tools/DOMMultiBlockTool.java
@@ -25,46 +25,26 @@ public class DOMMultiBlockTool extends MiningToolItem {
             Direction playerFacing = Direction.fromRotation(miner.getYaw());
             float verticalRotation = miner.getPitch();
 
-            if ((playerFacing == Direction.NORTH || playerFacing == Direction.SOUTH) && !(verticalRotation >= 45.0f || verticalRotation <= -45.0f )) {
                 for (int y = -radius; y <= radius; y++) {
-                    for (int x = -radius; x <= radius; x++) {
-                        for (int z = -0; z <= 0; z++) {
-                            BlockPos targetPos = pos.add(x, y, z);
-                            BlockState targetState = world.getBlockState(targetPos);
+                    for (int x = -radius; x <= radius; x++)  {
+                        BlockPos targetPos;
+                        if (verticalRotation >= 45.0f || verticalRotation <= -45.0f ) {
+                            targetPos = pos.add(x, 0, y);
+                        }
+                        else if (playerFacing == Direction.SOUTH || playerFacing == Direction.NORTH){
+                            targetPos = pos.add(y, x, 0);
+                        }
+                        else {
+                            targetPos = pos.add(0, y, -x);
+                        }
+                        BlockState targetState = world.getBlockState(targetPos);
 
-                            if (!world.isAir(targetPos) && targetState.getHardness(world, targetPos) >= 0) {
-                                world.breakBlock(targetPos, true, miner);
-                            }
+                        if (!world.isAir(targetPos) && targetState.getHardness(world, targetPos) >= 0) {
+                            world.breakBlock(targetPos, true, miner);
                         }
                     }
                 }
-            } else if ((playerFacing == Direction.EAST || playerFacing == Direction.WEST) && !(verticalRotation >= 45.0f || verticalRotation <= -45.0f )) {
-                for (int y = -radius; y <= radius; y++) {
-                    for (int z = -0; z <= 0; z++) {
-                        for (int x = -radius; x <= radius; x++) {
-                            BlockPos targetPos = pos.add(z, y, -x);
-                            BlockState targetState = world.getBlockState(targetPos);
 
-                            if (!world.isAir(targetPos) && targetState.getHardness(world, targetPos) >= 0) {
-                                world.breakBlock(targetPos, true, miner);
-                            }
-                        }
-                    }
-                }
-            } if (verticalRotation >= 45.0f || verticalRotation <= -45.0f ){ // playerFacing == Direction.DOWN or .UP
-                for (int x = -radius; x <= radius; x++) {
-                    for (int z = -0; z <= 0; z++) {
-                        for (int y = -radius; y <= radius; y++) {
-                            BlockPos targetPos = pos.add(x, z, y);
-                            BlockState targetState = world.getBlockState(targetPos);
-
-                            if (!world.isAir(targetPos) && targetState.getHardness(world, targetPos) >= 0) {
-                                world.breakBlock(targetPos, true, miner);
-                            }
-                        }
-                    }
-                }
-            }
         }
         return super.postMine(stack, world, state, pos, miner);
     }
